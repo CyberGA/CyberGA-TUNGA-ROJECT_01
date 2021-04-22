96c6077a4cda096fe43c4e685a373d82db74ab55
CyberGA <etomu.joshua@gmail.com>
2021-04-22 17:57:45 +0100

    added support for multiplication

--- index.js
+++ index.js
@@ -9,5 +9,12 @@ console.log(result);
 function subtraction(arg1, arg2) {
   return arg1 - arg2;
 }
-const result = subtraction(16, 8);
-console.log(result);
+const result1 = subtraction(16, 8);
+console.log(result1);
+
+//multiplication
+function multiplication(arg1, arg2) {
+  return arg1 * arg2;
+}
+const result2 = multiplication(4, 8);
+console.log(result2);
