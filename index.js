bab7f8b172d9ae4f7b60bc135ea64ec9ec2ab83a
CyberGA <etomu.joshua@gmail.com>
2021-04-22 17:36:54 +0100

    added support for subtraction

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+// addition
 function add(arg1, arg2) {
   return arg1 + arg2;
 }
-
 const result = add(8, 8);
+console.log(result);
 
+//subtraction
+function subtraction(arg1, arg2) {
+  return arg1 - arg2;
+}
+const result = subtraction(16, 8);
 console.log(result);
